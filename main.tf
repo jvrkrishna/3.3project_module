ece7d66e24a5b5d26fc38b81cded232e15213998
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-18 10:56:55 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,34 @@
 ########### Create ec2 instance in terraform #############
-resource "aws_instance" "web" {
+resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
     Name = "${var.component}-${var.env}"
+  }
+}
+
+############# Create vps security group in terraform ##############
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
   }
 }
