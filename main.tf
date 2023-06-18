7e8257c9a11d5d9e09365034337729f3ee3cde18
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-18 11:25:59 +0530

    terraform

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.id
   instance_type = "t2.micro"
-# vpc_security_group_ids = [aws_security_group.sg.id]
-
   tags = {
     Name = "${var.component}-${var.env}"
   }
