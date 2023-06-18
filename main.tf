c5bc1d97683e9be895c1cd60d949b02b7f092383
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-18 20:17:35 +0530

    terraform

--- main.tf
+++ main.tf
@@ -50,10 +50,6 @@ resource "aws_iam_role_policy_attachment" "policy-attach" {
   policy_arn = aws_iam_policy.policy.arn
 }
 
-
-
-
-
 ########### Create ec2 instance in terraform #############
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.id
